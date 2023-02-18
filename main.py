7b828781cb9bf4fec265869bdd5e48fbac16aec8
perkel <perkel@uw.edu>
2023-02-18 08:59:12 -0800

    added counting

--- main.py
+++ main.py
@@ -7,6 +7,8 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hello, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    for i in range(3):
+        print('Counting. i = ', i)
 
 
 # Press the green button in the gutter to run the script.
